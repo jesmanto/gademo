dba22d3434c63737b943cf1a99fb178994892b6d
jesmanto <okorojesse@gmail.com>
2025-03-09 17:12:09 +0100

    adding a new unit test to the tests

--- hello-world.test.js
+++ hello-world.test.js
@@ -11,7 +11,7 @@ describe('Node App API Test', () => {
   it('should respond with Hello World', async () => {
     const response = await request(app.server).get('/products');
     expect(response.statusCode).toEqual(200);
-    expect(response.text).toEqual('Hello World!');
+    expect(response.text).toEqual('<h1>This is the products page></h1>');
   });
 
   afterAll(() => {
