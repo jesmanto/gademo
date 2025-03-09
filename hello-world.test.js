9b32521a66dd10b54abeadec8808f77fefe4a1b8
jesmanto <okorojesse@gmail.com>
2025-03-09 12:21:58 +0100

    Adding express and test to the project

--- hello-world.test.js
+++ hello-world.test.js
@@ -3,12 +3,8 @@ const app = require('./index');
 
 describe('GET /', () => {
   it('should respond with Hello World', async () => {
-    const response = await request(app.server).get('/');
+    const response = await request(app).get('/');
     expect(response.statusCode).toEqual(200);
     expect(response.text).toEqual('Hello World!');
-  });
-
-  afterAll(() => {
-    app.server.close();
   });
 });
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-module.exports = {app, server};
+module.exports = app;
