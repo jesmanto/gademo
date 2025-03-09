18cd25019e7465f776d6060b2ba7b35fc4faad60
jesmanto <okorojesse@gmail.com>
2025-03-09 11:41:35 +0100

    Adding jest to the dependencies

--- hello-world.test.js
+++ hello-world.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('./index');
 
 describe('GET /', () => {
   it('should respond with Hello World', async () => {
     const response = await request(app).get('/');
+    expect(response.statusCode).toEqual(200);
     expect(response.text).toEqual('Hello World');
   });
 });
